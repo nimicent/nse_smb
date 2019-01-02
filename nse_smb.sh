9416e0000364ba285f62ae7a5b688bf0fcb6b665
nimicent <nimicent@protonmail.com>
2019-01-01 20:09:49 -0600

    :fire: outputs

--- nse_smb.sh
+++ nse_smb.sh
@@ -5,6 +5,6 @@ read IP
 
 echo NSE scripts for 445...
 
-nmap -v4 -p 445 --script=smb* --script-args=unsafe=1 "$IP" -T5 -d | tee "$IP"_445_nseScan.txt
+nmap -v4 -p 445 --script=smb* --script-args=unsafe=1 "$IP" -T5 -d -oG "$IP"_445_nseScan.txt
 
 echo Finished...
